4fedfc0146bc6aa96d17e65e4ba109d526ad3f8a
siL-rob <k_sung_a@naver.com>
2023-12-16 03:56:11 +0900

    feat: create f1 score
    #24

--- model/metric.py
+++ model/metric.py
@@ -1,4 +1,5 @@
 import torch
+from sklearn.metrics import f1_score
 
 
 def accuracy(output, target):
@@ -18,3 +19,11 @@ def top_k_acc(output, target, k=3):
         for i in range(k):
             correct += torch.sum(pred[:, i] == target).item()
     return correct / len(target)
+
+
+def calculate_f1_score(output, target):
+    with torch.no_grad():
+        pred = torch.argmax(output, dim=1)
+        assert pred.shape[0] == len(target) 
+        a=f1_score(target.cpu().numpy(), pred.cpu().numpy(), average='macro')
+    return f1_score(target.cpu().numpy(), pred.cpu().numpy(), average='macro')
