6a46b898e1b08a63de20b50275c17b5b25733df6
rudeuns <rudeuns@gmail.com>
2023-12-15 11:12:02 +0900

    feat: BasicDataLoader 구현
    #14

---
+++ data_loader/__init__.py
@@ -0,0 +1,3 @@
+from .data_loader import *
+from .train_dataset import *
+from .test_dataset import *
--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -1,16 +1,13 @@
-from torchvision import datasets, transforms
-from base import BaseDataLoader
+from torch.utils.data import DataLoader
 
+class BasicDataLoader(DataLoader):
+    def __init__(self, dataset, batch_size, shuffle, num_workers, sampler):
 
-class MnistDataLoader(BaseDataLoader):
-    """
-    MNIST data loading demo using BaseDataLoader
-    """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
-        self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+        self.init_kwargs = {
+            'dataset': dataset,
+            'batch_size': batch_size,
+            'shuffle': shuffle,
+            'num_workers': num_workers,
+            'sampler': sampler
+        }
+        super().__init__(**self.init_kwargs)
