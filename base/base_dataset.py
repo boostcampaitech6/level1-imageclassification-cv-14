1f2da2c90f45f35b21bcc321980a6b62fa758750
kyungeun <151593264+rudeuns@users.noreply.github.com>
2023-12-15 11:42:58 +0900

    feat: DataPipeline 구축 (#19)
    * feat: BaseDataset 구현
    
    #14
    
    * feat: TrainDataset 구현
    
    #14
    
    * feat: TestDataset 구현
    
    #14
    
    * feat: BasicDataLoader 구현
    
    #14
    
    * feat: data 관련 util 구현
    
    #14
    
    * chore: config.json 수정
    
    #14

---
+++ base/base_dataset.py
@@ -0,0 +1,110 @@
+import os
+from typing import Tuple
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset, Subset, random_split
+from utils import MaskLabels, GenderLabels, AgeLabels, encode_multi_class
+
+
+class BaseDataset(Dataset):
+    num_classes = 3 * 2 * 3
+
+    _file_names = {
+        "mask1": MaskLabels.MASK,
+        "mask2": MaskLabels.MASK,
+        "mask3": MaskLabels.MASK,
+        "mask4": MaskLabels.MASK,
+        "mask5": MaskLabels.MASK,
+        "incorrect_mask": MaskLabels.INCORRECT,
+        "normal": MaskLabels.NORMAL,
+    }
+
+    image_paths = []
+    mask_labels = []
+    gender_labels = []
+    age_labels = []
+
+    def __init__(
+        self, data_dir, val_ratio=0.2, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)
+    ):
+        self.data_dir = data_dir
+        self.val_ratio = val_ratio
+        self.mean = mean
+        self.std = std
+
+        self.setup()
+        self.calc_statistics()
+
+    def setup(self):
+        profiles = os.listdir(self.data_dir)
+        for profile in profiles:
+            if profile.startswith("."):
+                continue
+
+            img_folder = os.path.join(self.data_dir, profile)
+            for file_name in os.listdir(img_folder):
+                _file_name, ext = os.path.splitext(file_name)
+                if _file_name not in self._file_names:
+                    continue
+
+                img_path = os.path.join(self.data_dir, profile, file_name)
+                mask_label = self._file_names[_file_name]
+
+                id, gender, race, age = profile.split("_")
+                gender_label = GenderLabels.from_str(gender)
+                age_label = AgeLabels.from_number(age)
+
+                self.image_paths.append(img_path)
+                self.mask_labels.append(mask_label)
+                self.gender_labels.append(gender_label)
+                self.age_labels.append(age_label)
+
+    def load_image(self, index):
+        image = self.read_image(index)
+        return image
+    
+    def load_multi_class_label(self, index):
+        mask_label = self.get_mask_label(index)
+        gender_label = self.get_gender_label(index)
+        age_label = self.get_age_label(index)
+
+        multi_class_label = encode_multi_class(mask_label, gender_label, age_label)
+        return multi_class_label
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def get_mask_label(self, index) -> MaskLabels:
+        return self.mask_labels[index]
+
+    def get_gender_label(self, index) -> GenderLabels:
+        return self.gender_labels[index]
+
+    def get_age_label(self, index) -> AgeLabels:
+        return self.age_labels[index]
+
+    def read_image(self, index):
+        image_path = self.image_paths[index]
+        return Image.open(image_path)
+
+    def calc_statistics(self):
+        has_statistics = self.mean is not None and self.std is not None
+        if not has_statistics:
+            print(
+                "[Warning] Calculating statistics... It can take a long time depending on your CPU machine"
+            )
+            sums = []
+            squared = []
+            for image_path in self.image_paths[:3000]:
+                image = np.array(Image.open(image_path)).astype(np.int32)
+                sums.append(image.mean(axis=(0, 1)))
+                squared.append((image**2).mean(axis=(0, 1)))
+
+            self.mean = np.mean(sums, axis=0) / 255
+            self.std = (np.mean(squared, axis=0) - self.mean**2) ** 0.5 / 255
+		
+    def split_dataset(self) -> Tuple[Subset, Subset]:
+        n_val = int(len(self) * self.val_ratio)
+        n_train = len(self) - n_val
+        train_set, val_set = random_split(self, [n_train, n_val])
+        return train_set, val_set
