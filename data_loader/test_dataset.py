0afce54e7e95d0d30519c0445ce68df0e5da399a
kyungeun <151593264+rudeuns@users.noreply.github.com>
2023-12-15 19:19:26 +0900

    feat: albumentations 사용하여 transform 구현 (#26)
    * feat: PIL.Image로 반환되는 것을 image_path로 수정함
    
    #25
    
    * feat: albumentations 사용하여 transform 구현
    
    #25
    
    * feat: albumentations 사용하여 transform 구현
    
    #25
    
    * feat: cuda 사용시 pin_memory=True로 설정
    
    #25
    
    * docs: albumentations 추가
    
    #25
    
    * docs: opencv-python 추가
    
    #25

--- data_loader/test_dataset.py
+++ data_loader/test_dataset.py
@@ -1,30 +1,26 @@
 from torch.utils.data import Dataset
-from PIL import Image
-from torchvision.transforms import (
-    Resize,
-    ToTensor,
-    Normalize,
-    Compose,
-)
+import cv2
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 class TestDataset(Dataset):
     def __init__(
         self, img_paths, resize=224, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)
     ):
         self.img_paths = img_paths
-        self.transform = Compose(
-            [
-                Resize(resize, Image.BILINEAR),
-                ToTensor(),
-                Normalize(mean=mean, std=std),
-            ]
-        )
+        self.transform = A.Compose([
+            A.Normalize(mean, std),
+            A.Resize(resize, resize),
+            ToTensorV2()
+        ])
 
     def __getitem__(self, index):
-        image = Image.open(self.img_paths[index])
+        image_path = self.img_paths[index]
+        image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
 
         if self.transform is not None:
-            image = self.transform(image)
+            image = self.transform(image=image)['image']
+
         return image
 
     def __len__(self):
