7b6cbfa6210a1357fb4ccec2053bc1492c7a09c3
rudeuns <rudeuns@gmail.com>
2023-12-15 18:34:26 +0900

    feat: albumentations 사용하여 transform 구현
    #25

--- data_loader/train_dataset.py
+++ data_loader/train_dataset.py
@@ -1,33 +1,26 @@
 from base import BaseDataset
-from PIL import Image
-from torchvision.transforms import (
-    Resize,
-    ToTensor,
-    Normalize,
-    Compose,
-    CenterCrop,
-    ColorJitter,
-)
+import cv2
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 class TrainDataset(BaseDataset):
     def __init__(self, data_dir, val_ratio=0.2):
         super().__init__(data_dir, val_ratio)
 
-        self.transform = Compose(
-            [
-                CenterCrop((320, 256)),
-                Resize(224, Image.BILINEAR),
-                ColorJitter(0.1, 0.1, 0.1, 0.1),
-                ToTensor(),
-                Normalize(mean=self.mean, std=self.std),
-            ]
-        )
+        self.transform = A.Compose([   
+            A.Normalize(self.mean, self.std),
+            A.CenterCrop(400, 300),
+            A.Resize(224, 224),
+            ToTensorV2()
+        ])
 
     def __getitem__(self, index):
-        image = self.load_image(index)
+        image_path = self.load_image_path(index)
+        image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+
         multi_class_label = self.load_multi_class_label(index)
 
         if self.transform is not None:
-            image = self.transform(image)
+            image = self.transform(image=image)['image']
 
         return image, multi_class_label
