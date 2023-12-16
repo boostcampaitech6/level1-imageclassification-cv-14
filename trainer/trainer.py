80f9bb90b7baf9208a2cd1e065b57eac27d722f4
rudeuns <rudeuns@gmail.com>
2023-12-16 17:29:17 +0900

    feat: Trainer 수정
    - BasicTrainer에서 Trainer로 변경
    - Tensorboardwrite 관련 코드 삭제
    - fold값 전달받아 fold별 결과 출력
    - tqdm 사용하여 진행 상태 확인
    
    #36

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,36 +1,28 @@
-import numpy as np
 import torch
-from torchvision.utils import make_grid
 from base import BaseTrainer
-from utils import inf_loop, MetricTracker
+from utils import MetricTracker
+from tqdm import tqdm
 
 
-class BasicTrainer(BaseTrainer):
+class Trainer(BaseTrainer):
     """
     Trainer class
     """
-    def __init__(self, model, criterion, metrics, optimizer, config, device,
+    def __init__(self, model, criterion, metrics, optimizer, config, fold,
                  exp_name, exp_num, project_name, entity,
-                 train_loader, valid_loader=None, lr_scheduler=None, len_epoch=None):
-        super().__init__(model, criterion, metrics, optimizer, config,
+                 device, train_loader, valid_loader=None, lr_scheduler=None):
+        super().__init__(model, criterion, metrics, optimizer, config, fold,
                          exp_name, exp_num, project_name, entity,)
         self.config = config
         self.device = device
         self.train_loader = train_loader
-        if len_epoch is None:
-            # epoch-based training
-            self.len_epoch = len(self.train_loader)
-        else:
-            # iteration-based training
-            self.train_loader = inf_loop(train_loader)
-            self.len_epoch = len_epoch
         self.valid_loader = valid_loader
         self.do_validation = self.valid_loader is not None
         self.lr_scheduler = lr_scheduler
-        self.log_step = int(np.sqrt(train_loader.batch_size))
+        self.fold = fold
 
-        self.train_metrics = MetricTracker('loss', *[m.__name__ for m in self.metrics], writer=self.writer)
-        self.valid_metrics = MetricTracker('loss', *[m.__name__ for m in self.metrics], writer=self.writer)
+        self.train_metrics = MetricTracker('loss', *[m.__name__ for m in self.metrics], writer=None)
+        self.valid_metrics = MetricTracker('loss', *[m.__name__ for m in self.metrics], writer=None)
 
     def _train_epoch(self, epoch):
         """
@@ -42,7 +34,10 @@ class BasicTrainer(BaseTrainer):
         self.model.train()
         self.train_metrics.reset()
 
-        for batch_idx, (data, target) in enumerate(self.train_loader):
+        for batch_idx, (data, target) in enumerate(tqdm(
+            self.train_loader, 
+            desc="[Fold {} - Train Epoch {}]".format(self.fold, epoch)
+        )):
             data, target = data.to(self.device), target.to(self.device)
 
             self.optimizer.zero_grad()
@@ -51,20 +46,9 @@ class BasicTrainer(BaseTrainer):
             loss.backward()
             self.optimizer.step()
 
-            self.writer.set_step((epoch - 1) * self.len_epoch + batch_idx)
             self.train_metrics.update('loss', loss.item())
             for met in self.metrics:
                 self.train_metrics.update(met.__name__, met(output, target))
-
-            if batch_idx % self.log_step == 0:
-                self.logger.debug('Train Epoch: {} {} Loss: {:.6f}'.format(
-                    epoch,
-                    self._progress(batch_idx),
-                    loss.item()))
-                self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
-
-            if batch_idx == self.len_epoch:
-                break
 
         log = self.train_metrics.result()
 
@@ -74,6 +58,7 @@ class BasicTrainer(BaseTrainer):
 
         if self.lr_scheduler is not None:
             self.lr_scheduler.step()
+
         return log
 
     def _valid_epoch(self, epoch):
@@ -87,29 +72,17 @@ class BasicTrainer(BaseTrainer):
         self.valid_metrics.reset()
 
         with torch.no_grad():
-            for batch_idx, (data, target) in enumerate(self.valid_loader):
+            for batch_idx, (data, target) in enumerate(tqdm(
+                self.valid_loader, 
+                desc="[Fold {} - Valid Epoch {}]".format(self.fold, epoch)
+            )):
                 data, target = data.to(self.device), target.to(self.device)
 
                 output = self.model(data)
                 loss = self.criterion(output, target)
 
-                self.writer.set_step((epoch - 1) * len(self.valid_loader) + batch_idx, 'valid')
                 self.valid_metrics.update('loss', loss.item())
                 for met in self.metrics:
                     self.valid_metrics.update(met.__name__, met(output, target))
-                self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
 
-        # add histogram of model parameters to the tensorboard
-        for name, p in self.model.named_parameters():
-            self.writer.add_histogram(name, p, bins='auto')
         return self.valid_metrics.result()
-
-    def _progress(self, batch_idx):
-        base = '[{}/{} ({:.0f}%)]'
-        if hasattr(self.train_loader, 'n_samples'):
-            current = batch_idx * self.train_loader.batch_size
-            total = self.train_loader.n_samples
-        else:
-            current = batch_idx
-            total = self.len_epoch
-        return base.format(current, total, 100.0 * current / total)
