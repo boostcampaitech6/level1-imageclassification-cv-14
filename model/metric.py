1d8f95611af6eb72c196b7f1dc7b92a626f5a816
김실희_T6027 <58744783+siL-rob@users.noreply.github.com>
2023-12-16 16:56:24 +0900

    feat: F1 score 구현 (#32)
    * feat: create f1 score
    #24
    
    * chore: add f1 score
    #24
    
    * feat: modify metric.py
    #24
    
    * chore: modify config.json
    #24

--- model/metric.py
+++ model/metric.py
@@ -1,4 +1,6 @@
 import torch
+# from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score as calcualte_f1_score
 
 
 def accuracy(output, target):
@@ -18,3 +20,11 @@ def top_k_acc(output, target, k=3):
         for i in range(k):
             correct += torch.sum(pred[:, i] == target).item()
     return correct / len(target)
+
+
+def f1_score(output, target):
+    with torch.no_grad():
+        pred = torch.argmax(output, dim=1)
+        assert pred.shape[0] == len(target) 
+        a=calcualte_f1_score(target.cpu().numpy(), pred.cpu().numpy(), average='macro')
+    return calcualte_f1_score(target.cpu().numpy(), pred.cpu().numpy(), average='macro')
