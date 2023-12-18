fabc092e46c68f0aad7a1233c0e4be7579d64445
김정택_T6038 <beagentleman7@gmail.com>
2023-12-18 17:55:14 +0900

    feat: 데이터 증강 전처리 (#43)
    * feat: data preprocess
    
    - mask : incorrect : no mask = 5 : 5 : 5
    - young : middle : old = 1 : 1 : 1
    
    #39
    
    * fix: split file
    
    - split file into aug elder and aug no mask
    
    #39
    
    * fix: fix aug_nomask
    
    - add tqdm
    - fix error
    
    #39
    
    * style: change naming
    
    - mask1_.jpg -> mask.jpg
    
    #39
    
    * feat: aug elder
    
    - aug elder times 6
    
    #39
    
    * remove: preprocess.py
    
    - preprocess.py -> aug_nomask.py, aug_elder.py
    
    #39
    
    * refactor: styling
    
    - add tqdm
    - remove test args
    
    #39
    
    * feat: for comfortable
    
    - if you run data_aug.py than aug_nomask and aug_elder will run all together
    
    #39
    
    * refactor: change preprocess
    
    - [['blur','flip','jitter'], ['flip','jitter'], ['jitter'], ['blur', 'jitter'], ['flip', 'jitter']]
    
    #39
    
    * refactor: changes in tqdm
    
    #39
    
    * refactor: spacing for operator
    
    #39
    
    * refactor: change naming var
    
    #39
    
    * refacotring: refactor aug_nomask
    
    - remove categorize_file and add it to setup method
    - change src_path -> src_img_path, dest_path -> dest_img_path
    
    #39
    
    * refactor: change suffix style
    
    #39
    
    * refactor: change var name
    
    #39
    
    * refactor: change var name fun
    
    #39
    
    * refactor: pathes -> paths
    
    #39
    
    * refactor: refactor aug_elder
    
    - fun -> func
    - remove aug_data
    - change name aug_elder_data -> aug_data
    
    #39
    
    * refactor: " -> \' for integration
    
    #39
    
    * refactor: change method order
    
    #39
    
    * refactor: reorder
    
    - reorder method for user
    
    #39
    
    * refactor: code refactor
    
    - remove aug_data.py
    
    #39

---
+++ utils/aug_elder.py
@@ -0,0 +1,144 @@
+import os
+import cv2
+import random
+import argparse
+import multiprocessing
+from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
+
+class AugElder():
+    def __init__(self, src_dir):
+        self.src_dir = src_dir
+        self.n_cpu = multiprocessing.cpu_count()
+
+        # params
+        self.brightness = 64
+        self.contrast = 64
+
+        # new id
+        self.new_id = 7000
+
+        # dir path init
+        self.src_dirs = []
+        self.dest_dirs = []
+
+        self.src_files_paths = []
+        self.dest_files_paths = []
+
+        # setup
+        self.setup()
+
+    def setup(self):
+        profiles = [p for p in os.listdir(self.src_dir) if not p.startswith('.')]
+
+        for profile in profiles:
+            src_profile_dir = os.path.join(self.src_dir, profile)
+            id, gender, race, age = profile.split("_")
+
+            if int(age)<60: continue
+
+            self.handle_elder_profiles(src_profile_dir, gender, race, age)
+
+        for i in range(len(self.src_dirs)):
+            for file_name in os.listdir(self.src_dirs[i]):
+                self.categorize_file(self.src_dirs[i], self.dest_dirs[i], file_name)
+
+    def handle_elder_profiles(self, src_dir, gender, race, age):
+        self.src_dirs.append(src_dir)
+        duplicated = []
+
+        for _ in range(5):
+            elder_id = str(self.new_id).zfill(6)
+            self.new_id += 1
+
+            dest_elder_profile = '_'.join([elder_id, gender, race, age])
+            dest_elder_dir = os.path.join(self.src_dir, dest_elder_profile)
+            self.makefolder(dest_elder_dir)
+            duplicated.append(dest_elder_dir)
+        self.dest_dirs.append(duplicated)
+
+    def makefolder(self, new_dir_path):
+        os.makedirs(new_dir_path, exist_ok = False)
+    
+    def categorize_file(self, src_dir, dest_dirs, file_name):
+        _file_name, ext = os.path.splitext(file_name)
+        src_file_path = os.path.join(src_dir, file_name)
+        
+        dest_files_paths = []
+        for dest_dir in dest_dirs:
+            dest_files_paths.append(os.path.join(dest_dir,_file_name))
+        
+        self.src_files_paths.append(src_file_path)
+        self.dest_files_paths.append(dest_files_paths)
+
+    def aug_data(self):
+        process_types = [['blur','flip','jitter'], ['flip','jitter'], ['jitter'], ['blur', 'jitter'], ['flip', 'jitter']]
+        b_list = [b for b in range(0,65,13)]
+        c_list = [c for c in range(0,65,13)]
+        b_c_comb = [(b,c) for b in b_list for c in c_list]
+        b_c_comb = random.sample(b_c_comb, 5)
+
+        tasks = [[],[],[]]
+        for i in range(len(self.src_files_paths)):
+            src_file_path = self.src_files_paths[i]
+            dest_files_path = self.dest_files_paths[i]
+            
+            for j in range(5):
+                funcs = process_types[j]
+                b, c = b_c_comb[j]
+                dest_file_path = dest_files_path[j]
+                self.brightness = b
+                self.contrast = c
+                suffix = '_' + '_'.join(funcs) + f'_b{b}_c{c}.jpg'
+                dest_file_path += suffix
+                tasks[0].append(src_file_path)
+                tasks[1].append(dest_file_path)
+                tasks[2].append(funcs)
+
+        self.multiple_process(tasks[0], tasks[1], tasks[2])
+
+    def multiple_process(self,src_list, dest_list, n_funcs):
+        with ProcessPoolExecutor(max_workers = self.n_cpu) as executor:
+            list(tqdm(executor.map(self.single_process, src_list, dest_list, n_funcs), total = len(src_list)))
+
+    def single_process(self, src_img_path, dest_img_path, funcs):
+        img = self.get_img(src_img_path)
+        for func in funcs :
+            img = getattr(self,func)(img)
+        cv2.imwrite(dest_img_path, img)
+
+    def get_img(self, img_path):
+        return cv2.imread(img_path)
+
+    def blur(self, img):
+        return cv2.GaussianBlur(img, (0,0), sigmaX = 3)
+
+    def flip(self, img):
+        return cv2.flip(img, 1)
+
+    # https://stackoverflow.com/questions/39308030/how-do-i-increase-the-contrast-of-an-image-in-python-opencv 
+    def jitter(self, img):
+        brightness, contrast = self.brightness, self.contrast
+        if brightness != 0:
+            shadow = brightness if brightness > 0 else 0
+            highlight = 255 if brightness > 0 else 0
+            img = cv2.convertScaleAbs(img, alpha = (highlight - shadow)/255, beta = shadow)
+
+        if contrast != 0:
+            f = 131*(contrast + 127)/(127*(131-contrast))
+            img = cv2.convertScaleAbs(img, alpha = f, beta = 127*(1-f))
+
+        return img
+
+def main(src_dir):
+    aug_data = AugElder(src_dir)
+    aug_data.aug_data()
+    print('Data augmentation completed.')
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description = 'data preprocessing')
+    args.add_argument('-d', '--src_dir', default = './data/train/images_aug', type = str,
+                      help='data folder path (default: ./data/train)')
+    
+    args = args.parse_args()
+    main(args.src_dir)
---
+++ utils/aug_nomask.py
@@ -0,0 +1,115 @@
+import os
+import cv2
+import argparse
+import multiprocessing
+from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
+
+class AugNoMask():
+    def __init__(self, src_dir, dest_dir, brightness, contrast):
+        self.src_dir = src_dir
+        self.dest_dir = dest_dir
+        self.n_cpu = multiprocessing.cpu_count()
+
+        # params
+        self.brightness = brightness
+        self.contrast = contrast
+
+        # categorical path init
+        self.src_mask_paths = []
+        self.src_normal_paths = []
+        self.src_incorrect_paths = []
+        self.dest_mask_paths = []
+        self.dest_normal_paths = []
+        self.dest_incorrect_paths = []
+
+        # setup
+        self.setup()
+
+    def setup(self):
+        profiles = [p for p in os.listdir(self.src_dir) if not p.startswith('.')]
+
+        for profile in profiles:
+            src_profile_dir = os.path.join(self.src_dir, profile)
+            dest_profile_dir = os.path.join(self.dest_dir, profile)
+            self.makefolder(dest_profile_dir)
+
+            for file_name in os.listdir(src_profile_dir):
+                _file_name, ext = os.path.splitext(file_name)
+                src_img_path = os.path.join(src_profile_dir, file_name)
+                dest_img_path = os.path.join(dest_profile_dir, _file_name)
+
+                if _file_name.startswith('mask'):
+                    self.src_mask_paths.append(src_img_path)
+                    self.dest_mask_paths.append(dest_img_path)
+                elif _file_name.startswith('normal'):
+                    self.src_normal_paths.append(src_img_path)
+                    self.dest_normal_paths.append(dest_img_path)
+                elif _file_name.startswith('incorrect'):
+                    self.src_incorrect_paths.append(src_img_path)
+                    self.dest_incorrect_paths.append(dest_img_path)
+
+    def makefolder(self, new_dir_path):
+        os.makedirs(new_dir_path, exist_ok=True)
+
+    
+    def single_process(self, src_img_path, dest_img_path, funcs):
+        img = self.get_img(src_img_path)
+        if funcs != [''] :
+            for func in funcs :
+                img = getattr(self,func)(img)
+        cv2.imwrite(dest_img_path, img)
+
+    def multiple_process(self, src_img_paths, dest_img_paths, n_funcs):
+        with ProcessPoolExecutor(max_workers=self.n_cpu) as executor:
+            list(tqdm(executor.map(self.single_process, src_img_paths, dest_img_paths, n_funcs), total=len(src_img_paths)))
+
+    def aug_data(self):
+        process_types = [[''], ['blur'], ['flip'], ['jitter'], ['blur', 'flip']]
+
+        for src_img_paths, dest_img_paths, category in ([(self.src_mask_paths, self.dest_mask_paths, 'mask'),
+                                                 (self.src_normal_paths, self.dest_normal_paths, 'normal'),
+                                                 (self.src_incorrect_paths, self.dest_incorrect_paths, 'incorrect')]):
+            process = [['']] if category == 'mask' else process_types
+            for funcs in process:
+                suffix = '_'.join(funcs)
+                mod_dest_img_paths = [p + f'_{suffix}.jpg' for p in dest_img_paths]
+                self.multiple_process(src_img_paths, mod_dest_img_paths, [funcs for _ in range(len(src_img_paths))])
+
+    def get_img(self, img_path):
+        return cv2.imread(img_path)
+
+    def blur(self, img):
+        return cv2.GaussianBlur(img, (0,0), sigmaX=3)
+
+    def flip(self, img):
+        return cv2.flip(img, 1)
+
+    # https://stackoverflow.com/questions/39308030/how-do-i-increase-the-contrast-of-an-image-in-python-opencv 
+    def jitter(self, img):
+        brightness, contrast = self.brightness, self.contrast
+        if brightness != 0:
+            shadow = brightness if brightness > 0 else 0
+            highlight = 255 if brightness > 0 else 0
+            img = cv2.convertScaleAbs(img, alpha=(highlight - shadow)/255, beta=shadow)
+
+        if contrast != 0:
+            f = 131*(contrast + 127)/(127*(131-contrast))
+            img = cv2.convertScaleAbs(img, alpha=f, beta=127*(1-f))
+
+        return img
+
+def main(src_dir, dest_dir):
+    aug_data = AugNoMask(src_dir, dest_dir, brightness=64, contrast=64)
+    aug_data.aug_data()
+    print('Data augmentation completed.')
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='data preprocessing')
+    args.add_argument('-s', '--src_dir', default='./data/train/images', type=str,
+                      help='src data folder path (default: ./data/train/images)')
+    args.add_argument('-d', '--dest_dir', default='./data/train/images_aug', type=str,
+                      help='add folder name to aug ver data folder')
+
+    args = args.parse_args()
+    main(args.src_dir, args.dest_dir)
