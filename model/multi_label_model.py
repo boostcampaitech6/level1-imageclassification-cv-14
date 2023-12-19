3b86254a08f3461bbcbcb2c20d79de683ef8f01c
Jungtaxi <beagentleman7@gmail.com>
2023-12-19 13:57:14 +0900

    fix: change classifier
    - take full vgg19 model and change last layer
    
    #51

--- model/multi_label_model.py
+++ model/multi_label_model.py
@@ -18,15 +18,14 @@ class MultiLabelEfficientB0(BaseModel):
 class MultiLabelVGG19(BaseModel):
     def __init__(self, num_classes=18):
         super().__init__()
-        self.pretrained_model = models.vgg19(weights=models.VGG19_Weights.DEFAULT).features
+        self.pretrained_model = models.vgg19(weights=models.VGG19_Weights.DEFAULT)
+        
         for param in self.pretrained_model.parameters():
             param.requires_grad = False
+        
+        in_features = self.pretrained_model.classifier[-1].in_features
 
-        self.classifier = nn.Sequential(
-            nn.Linear(4096, 8)
-        )
-
+        self.pretrained_model.classifier[-1] = nn.Linear(in_features, 8)
+    
     def forward(self, x):
-        x = self.pretrained_model(x)
-        x = self.classifier(x.view(x.size(0),-1))
-        return x
+        return self.pretrained_model(x)
