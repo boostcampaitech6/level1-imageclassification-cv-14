1e202848301fe91482ef4b4b6aecbc4397ca26e2
rudeuns <rudeuns@gmail.com>
2023-12-21 01:48:26 +0900

    feat: Modify train.py main func
    #78

--- train.py
+++ train.py
@@ -4,19 +4,17 @@ import argparse
 import collections
 import torch
 import torch.utils.data as torch_utils
-from torch.utils.data import Subset, ConcatDataset
-from sklearn.model_selection import StratifiedKFold
 import data_loader as module_data
 import model as module_arch
 import model.loss as module_loss
 import model.metric as module_metric
 import trainer as module_trainer
 from parse_config import TrainConfigParser
-from utils import prepare_device, encode_multi_class
+from utils import prepare_device
 
 
 # fix random seeds for reproducibility
-SEED = 42
+SEED = 951
 torch.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
@@ -26,66 +24,45 @@ random.seed(SEED)
 def main(config):
     logger = config.get_logger('train')
 
-    datasets, labels = [], []
-    for data_dir in config['dataset']['data_dir']:
-        _dataset = config.init_obj('dataset', module_data, data_dir)
-        _labels = [
-            encode_multi_class(mask, gender, age)
-            for mask, gender, age in zip(
-                _dataset.mask_labels, _dataset.gender_labels, _dataset.age_labels
-            )
-        ]
+    train_set = config.init_obj('train_set', module_data, logger=logger)
+    valid_set = config.init_obj('valid_set', module_data)
 
-        datasets.append(_dataset)
-        labels += _labels
+    train_sampler = torch_utils.RandomSampler(train_set)
+    valid_sampler = torch_utils.SequentialSampler(valid_set)
 
-    dataset = ConcatDataset(datasets)
-    
-    k_splits = config['k_splits']
-    skf = StratifiedKFold(n_splits=k_splits, shuffle=True, random_state=SEED)
+    train_loader = config.init_obj('train_loader', module_data, train_set, sampler=train_sampler)
+    valid_loader = config.init_obj('valid_loader', module_data, valid_set, sampler=valid_sampler)
 
-    results = []
-    for fold, (train_ids, valid_ids) in enumerate(skf.split(dataset, labels)):
-        train_subset = Subset(dataset, train_ids)
-        valid_subset = Subset(dataset, valid_ids)
+    model = config.init_obj('arch', module_arch)
+    logger.info(model)
 
-        train_sampler = torch_utils.RandomSampler(train_subset)
-        valid_sampler = torch_utils.SequentialSampler(valid_subset)
+    device, device_ids = prepare_device(config['n_gpu'])
+    model = model.to(device)
 
-        train_loader = config.init_obj('train_loader', module_data, train_subset, sampler=train_sampler)
-        valid_loader = config.init_obj('valid_loader', module_data, valid_subset, sampler=valid_sampler)
+    if len(device_ids) > 1:
+        model = torch.nn.DataParallel(model, device_ids=device_ids)
 
-        model = config.init_obj('arch', module_arch)
-        logger.info(model)
+    criterion = getattr(module_loss, config['loss'])
+    metrics = [getattr(module_metric, met) for met in config['metrics']]
 
-        device, device_ids = prepare_device(config['n_gpu'])
-        model = model.to(device)
-        if len(device_ids) > 1:
-            model = torch.nn.DataParallel(model, device_ids=device_ids)
+    trainable_params = filter(lambda p: p.requires_grad, model.parameters())
+    optimizer = config.init_obj('optimizer', torch.optim, trainable_params)
+    lr_scheduler = config.init_obj('lr_scheduler', torch.optim.lr_scheduler, optimizer)
 
-        criterion = getattr(module_loss, config['loss'])
-        metrics = [getattr(module_metric, met) for met in config['metrics']]
+    trainer_kwargs = {
+        'model': model,
+        'criterion': criterion,
+        'metrics': metrics,
+        'optimizer': optimizer,
+        'config': config,
+        'device': device,
+        'train_loader': train_loader,
+        'valid_loader': valid_loader,
+        'lr_scheduler': lr_scheduler
+    }
+    trainer = config.init_obj('trainer', module_trainer, **trainer_kwargs)
 
-        trainable_params = filter(lambda p: p.requires_grad, model.parameters())
-        optimizer = config.init_obj('optimizer', torch.optim, trainable_params)
-        lr_scheduler = config.init_obj('lr_scheduler', torch.optim.lr_scheduler, optimizer)
-
-        trainer_kwargs = {
-            'model': model,
-            'criterion': criterion,
-            'metrics': metrics,
-            'optimizer': optimizer,
-            'config': config,
-            'fold': fold + 1,
-            'device': device,
-            'train_loader': train_loader,
-            'valid_loader': valid_loader,
-            'lr_scheduler': lr_scheduler
-        }
-        trainer = config.init_obj('trainer', module_trainer, **trainer_kwargs)
-
-        result = trainer.train()
-        results.append(result)
+    trainer.train()
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser(description='PyTorch Template')
