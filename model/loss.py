551f126c1986178c154dc6340502ceed46e9ec68
김정택_T6038 <beagentleman7@gmail.com>
2023-12-26 17:06:41 +0900

    feat: add loss funcs (#90)
    * feat: add loss funcs
    
    #89
    
    * refactor: rename method
    
    - get prefix of cross entropy loss
    
    #89
    
    * refactor: change 1/2 -> 0.5
    
    - 1/2 -> 0.5
    - There was no need to calculate it twice, so I changed it to the following.
    
    #89

--- model/loss.py
+++ model/loss.py
@@ -89,3 +89,21 @@ def f1_loss(outputs, targets):
 
 def label_smoothing_loss(outputs, targets):
     return LabelSmoothingLoss()(outputs, targets)
+
+def f1_focal_loss(outputs, targets):
+    return  0.5*F1Loss()(outputs, targets) + 0.5*FocalLoss()(outputs, targets)
+
+def f1_label_loss(outputs, targets):
+    return  0.5*F1Loss()(outputs, targets) + 0.5*LabelSmoothingLoss()(outputs, targets)
+
+def f1_cross_loss(outputs, targets):
+    return  0.5*F1Loss()(outputs, targets) + 0.5*F.cross_entropy(outputs, targets)
+
+def focal_cross_loss(outputs, targets):
+    return  0.5*FocalLoss()(outputs, targets) + 0.5*F.cross_entropy(outputs, targets)
+
+def focal_label_loss(outputs, targets):
+    return  0.5*FocalLoss()(outputs, targets) + 0.5*LabelSmoothingLoss()(outputs, targets)
+
+def label_cross_loss(outputs, targets):
+    return 0.5*LabelSmoothingLoss()(outputs, targets) + 0.5*F.cross_entropy(outputs, targets)
